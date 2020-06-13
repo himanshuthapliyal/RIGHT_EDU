c8b19f4c1bb95597f5dad0424ed49cc484fffa38
himanshuthapliyal <40329682+himanshuthapliyal@users.noreply.github.com>
2020-06-13 19:01:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo ' <marquee> WECOME TO MY CLOUD WORLD </marquee> ';
 <?php
 echo' 
 <html>
-<center> <img src="https://github.com/himanshuthapliyal/CMS.git"  width="300" height="400">  </center>
+<center> <img src="https://raw.githubusercontent.com/himanshuthapliyal/RIGHT_EDU/master/NmDIPyO_.jpg"  width="300" height="400">  </center>
 </html>
 ';
 
