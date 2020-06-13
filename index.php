6280ae59d3e205224bc2b6c4676882b6872786e5
himanshuthapliyal <40329682+himanshuthapliyal@users.noreply.github.com>
2020-06-13 17:38:48 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+echo ' <marquee> WECOME TO MY CLOUD WORLD </marquee> ';
+<br />
+
+
+
+<pre>
+<?php
+echo' 
+<html>
+<center> <img src="https://github.com/Himjo/terra-image.git"  width="300" height="400">  </center>
+</html>
+';
+
+
+
+?>
+</pre>
